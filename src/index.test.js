ec6811473e0b8c5ae05997e25df71f25361a4331
Kent C. Dodds <kent@doddsfamily.us>
2015-08-18 21:08:13 -0600

    Adding tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,26 @@ var expect = require('chai').expect;
 var starWars = require('./index');
 
 describe('starwars-names', function() {
-  it('should work!', function() {
-    expect(true).to.be.true;
+  describe('all', function() {
+    it('should be an array of strings', function() {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('should contain `Luke Skywalker`', function() {
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
+  });
+
+  describe('random', function() {
+    it('should return a random item from the starWars.all', function() {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
   });
 });
