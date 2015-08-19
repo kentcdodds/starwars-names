ca90b41baaf4eacac4d8888f72a5f050140abba9
Kent C. Dodds <kent@doddsfamily.us>
2015-08-18 21:01:29 -0600

    Setting up mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('starwars-names', function() {
+  it('should work!', function() {
+    expect(true).to.be.true;
+  });
+});
