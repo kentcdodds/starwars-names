defcd6cb0e780372478bd2bb580fb7e038f390af
Kent C. Dodds <kent@doddsfamily.us>
2015-08-22 20:34:59 -0600

    chore(test): Add ES6 support to tests
    Alter test scripts with mocha using --compiler ✨

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
-var expect = require('chai').expect;
-var starWars = require('./index');
+import {expect} from 'chai';
+import starWars from './index';
 
 describe('starwars-names', function() {
   describe('all', function() {
