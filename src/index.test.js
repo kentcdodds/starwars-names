e54c9354b6603e3f59364ca59fb70d2089f515a8
Kent C. Dodds <kent@doddsfamily.us>
2016-07-06 07:14:42 -0600

    write tests 📝

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,26 @@
 'use strict';
 
 var expect = require('chai').expect;
+var starWarsNames = require('.');
 
 describe('starwars-names', function() {
-  it('should work', function() {
-    expect(true).to.be.true
+  it('should have a list of all available names', function() {
+    expect(starWarsNames.all).to.satisfy(isArrayOfStrings);
+  });
+
+  it('should allow me to get a random name from the list', function() {
+    expect(starWarsNames.random()).to.satisfy(isIncludedIn(starWarsNames.all));
   });
 });
+
+function isArrayOfStrings(array) {
+  return array.every(function(i) {
+    return typeof i === 'string';
+  });
+}
+
+function isIncludedIn(array) {
+  return function(item) {
+    return array.includes(item);
+  };
+}
