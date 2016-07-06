dc6bdc8b2706b43bec8ce8f972eb12e914cd6808
Kent C. Dodds <kent@doddsfamily.us>
2016-07-05 20:39:04 -0600

    add dependencies and create module 📝

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var uniqueRandomArray = require('unique-random-array');
+var starWarsNames = require('./starwars-names.json');
+
+module.exports = {
+  all: starWarsNames,
+  random: uniqueRandomArray(starWarsNames)
+};
