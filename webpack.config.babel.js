dd2c20a060cf24f43b24e3fc9e686009b15aec23
Kent C. Dodds <kent@doddsfamily.us>
2016-07-09 23:13:57 -0600

    add webpack ✨

---
+++ webpack.config.babel.js
@@ -0,0 +1,20 @@
+import {join} from 'path'
+
+const context = join(__dirname, 'src')
+
+export default {
+  context,
+  entry: './index',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'starWarsNames',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {test: /\.js$/, loaders: ['babel'], include: context},
+      {test: /\.json$/, loaders: ['json'], include: context},
+    ],
+  },
+}
