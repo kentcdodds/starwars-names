e8f1be852a76f2b05fd675b157f499c8c5ea70b8
Kent C. Dodds <kent@doddsfamily.us>
2016-07-09 22:42:59 -0600

    transpile tests 📚

--- src/index.test.js
+++ src/index.test.js
@@ -1,26 +1,20 @@
-'use strict';
+import {expect} from 'chai'
+import starWarsNames from '.'
 
-var expect = require('chai').expect;
-var starWarsNames = require('.');
+describe('starwars-names', () => {
+  it('should have a list of all available names', () => {
+    expect(starWarsNames.all).to.satisfy(isArrayOfStrings)
+  })
 
-describe('starwars-names', function() {
-  it('should have a list of all available names', function() {
-    expect(starWarsNames.all).to.satisfy(isArrayOfStrings);
-  });
-
-  it('should allow me to get a random name from the list', function() {
-    expect(starWarsNames.random()).to.satisfy(isIncludedIn(starWarsNames.all));
-  });
-});
+  it('should allow me to get a random name from the list', () => {
+    expect(starWarsNames.random()).to.satisfy(isIncludedIn(starWarsNames.all))
+  })
+})
 
 function isArrayOfStrings(array) {
-  return array.every(function(i) {
-    return typeof i === 'string';
-  });
+  return array.every(i => typeof i === 'string')
 }
 
 function isIncludedIn(array) {
-  return function(item) {
-    return array.includes(item);
-  };
+  return item => array.includes(item)
 }
